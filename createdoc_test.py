f3b2714b1f52764467ec407fe656f8af41e4024c
Nick-M1 <nicholasmalloura@gmail.com>
2022-12-24 14:58:42 +0000

    updated requirements.txt

---
+++ createdoc_test.py
@@ -0,0 +1,5 @@
+import subprocess
+
+# subprocess.run('python -c "import sys; print(sys.path)"')
+
+subprocess.run('python createdoc.py -i input_files/HarryPotter_Book1_ThePhilosophersStone.txt -n "Harry Potter - Philosophers Stone"')
